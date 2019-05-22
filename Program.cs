8f97052aba1f58513a53dc6aa095651910919373
123 <123@DESKTOP-FHCEKTT>
2019-05-22 20:13:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=1, b=2, z=0;
+            if (b == 0)
+            {
+                
+                Console.WriteLine("No solutins");
+            }          
+            else
+            {
+                z = a /b;
+                Console.WriteLine(z);
+            }
+
+        }   
+    }
+}
